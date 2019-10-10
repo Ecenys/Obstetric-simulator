8248ee6466287c6da991d2f2833e4abf3b38f96c
Oscar Gomez Monedero <elcelestial97@outlook.com>
2019-10-10 14:28:13 +0200

    Modify colors in main manu

---
+++ Assets/Scripts/Selector/IconMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconMove : MonoBehaviour
+{
+
+    private double time;
+    private bool sequence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sequence = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 7) { //seconds to change the rotation
+            sequence = !sequence;
+            time = 0;
+        }
+        if (sequence)
+            transform.Rotate(Vector3.back, Time.deltaTime * 2); //velocity
+        else
+            transform.Rotate(Vector3.forward, Time.deltaTime * 2);
+    }
+}
