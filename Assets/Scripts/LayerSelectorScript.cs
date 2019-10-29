d58076d6fcbc624832f7360a96a6d5d09cb61beb
Oscar Gomez Monedero <elcelestial97@outlook.com>
2019-10-29 19:34:36 +0100

    Added funcionality in simulation layer, and added more types of uterus and babys

---
+++ Assets/Scripts/LayerSelectorScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSelectorScript : MonoBehaviour
+{
+    private static int layer;
+    private static int maxLayer = 3;
+    private static int minLayer = 0;
+
+    private void Update()
+    {
+        Debug.Log(layer);
+    }
+
+    public void Mas() {
+        if (layer != maxLayer)
+        {
+            switch (layer)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            layer++;
+        }
+    }
+
+    public void Menos() {
+        if (layer != minLayer) {
+            switch (layer) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            layer--;
+        }
+    }
+}
